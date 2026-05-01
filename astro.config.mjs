2806993fbab25f48ae88db9944c7297b684e0fd8
Andjelko Cvjetkovic <andjelko.cvjetkovic@codepool.io>
2026-05-01 09:53:56 +0200

    Build resume page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    icon({
+      include: {
+        lucide: ['mail', 'github', 'linkedin', 'map-pin'],
+      },
+    }),
+  ],
   adapter: vercel(),
   devToolbar: {
     enabled: false,
